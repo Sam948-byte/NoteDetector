bf4ad7018f4d33b523a08b4f8bab8c67b90b502c
Sam948-byte <techguy763@gmail.com>
2024-01-29 11:34:21 -0600

    update predict script

--- scripts/predict.py
+++ scripts/predict.py
@@ -1,10 +1,40 @@
+import cv2
 from ultralytics import YOLO
 
-# Load a pretrained YOLOv8n model
-model = YOLO('/home/sam/gitClones/NoteDetector/best.pt')
+# Load the YOLOv8 model
+model = YOLO('best.pt')
 
-# Define path to video file
-source = '/home/sam/gitClones/NoteDetector/videos/2024 Field Tour Video_ Source.mp4'
+# Open the video file
+video_source = "videos/2024 Field Tour Video_ Source.mp4"
+video_speaker = "videos/2024 Field Tour Video_ Speaker.mp4"
+cap = cv2.VideoCapture(video_speaker)
 
-# Run inference on the source
-results = model(source, stream=True, show=True)  # generator of Results objects
+# Loop through the video frames
+while cap.isOpened():
+    # Read a frame from the video
+    success, frame = cap.read()
+
+    if success:
+        # Run YOLOv8 inference on the frame
+        results = model(frame)
+        
+        # rename to note due to wrong labeling
+        for result in results:
+            result.names[1] = "note"
+
+        # Visualize the results on the frame
+        annotated_frame = results[0].plot()
+
+        # Display the annotated frame
+        cv2.imshow("YOLOv8 Inference", annotated_frame)
+
+        # Break the loop if 'q' is pressed
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    else:
+        # Break the loop if the end of the video is reached
+        break
+
+# Release the video capture object and close the display window
+cap.release()
+cv2.destroyAllWindows()
